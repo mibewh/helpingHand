f53a2ba9823668dc0ed95e01b134dd3986d44ead
Michael Whitman <mwhitman95@gmail.com>
2015-10-17 14:45:02 -0500

    Test

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.debug=True
 
 @app.route('/')
-def hello_world():
-
-	return render_template('index.jade', testvar='hiya')
-
+def hello():
+    return 'Hello World!'
