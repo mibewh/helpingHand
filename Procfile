6818f67aa3e7bbf5b8937c03db7ae8a12b9ac516
Michael Whitman <mwhitman95@gmail.com>
2015-10-17 15:15:27 -0500

    Stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app --log-file=-
+web: gunicorn test:app --log-file=-
--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-#app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.debug=True
 
 @app.route('/')
