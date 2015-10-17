6057e5aacdfda5146235a5be5db3d31cf1d3cc1b
Michael Whitman <mwhitman95@gmail.com>
2015-10-16 22:57:04 -0500

    Work on some basic templates

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+app.debug=True
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+
+	return render_template('index.jade', testvar='hiya')
 
 if __name__ == '__main__':
 	app.run()
