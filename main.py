0ee974c0afb1a43fc6846a8e8e8799b00e371fc0
Michael Whitman <mwhitman95@gmail.com>
2015-10-18 23:30:56 -0500

    Searching

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ db = SQLAlchemy(app)
 db.engine.connect() 
 
 from user import users
-from request import request
+from request import requestsBP
 app.register_blueprint(users)
-app.register_blueprint(request)
+app.register_blueprint(requestsBP)
 
 @app.route('/')
 def index():
--- request.py
+++ request.py
@@ -2,22 +2,34 @@ from flask import Flask, render_template, g, redirect, request, session, flash, 
 from sqlalchemy.sql import text
 from main import db
 
-request = Blueprint('request', __name__)
+requestsBP = Blueprint('request', __name__)
 
-@request.route('/create', methods=('GET', 'POST'))
+@requestsBP.route('/create', methods=('GET', 'POST'))
 def create_service_request():
 	if request.method == 'POST':
-		sql = text('''insert into service_request(client_id, address, title, decsription, schedule)values('{0}', '{1}', '{2}');'''\
-		.format(session["user"], request.form.get('address'), request.form.get('title'), request.form.get('description'), request.form.get("time"))
+		sql = text('''insert into service_request(client_username, address, title, description, schedule)
+						values('{0}', '{1}', '{2}', '{3}', '{4}');'''\
+		.format(session["user"], request.form.get('address'), request.form.get('title'), request.form.get('description'), request.form.get("time")))
 		db.engine.execute(sql)
 		return redirect('/')
+	if session['user']:
+		return render_template('create.jade')
+	else:
+		return redirect('/')
 
-@request.route('/', methods=('GET', 'POST'))
+@requestsBP.route('/search', methods=('GET', 'POST'))
 def searchName():
 	if request.method == 'POST':
-		search = request.form.get('search')
-
-		sql = text('''"sp_awesome_search_service_requests"('{0}')'''.format(search)
-
+		search = request.form.get('query')
+		print(search)
+		#sql = text('''select "sp_awesome_search_service_requests"('{0}')'''.format(search))
+		sql = text('''SELECT sr.title, sr.description, sr.client_username, sr.schedule, sr.address
+						FROM service_request sr
+						WHERE
+							sr.title LIKE '%'||'{0}'||'%' OR
+							sr.description LIKE '%'||'{0}'||'%' OR
+							sr.address LIKE '%'||'{0}'||'%';'''.format(search))
 		results = db.engine.execute(sql)
-		return results
+		results = results.fetchall()
+		return render_template('searchRequests.jade', results=results)
+	return render_template('searchRequests.jade')
