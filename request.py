3f0064b4e24a529c58cb377e4414d11221f2b240
Vince <vcap51@yahoo.com>
2015-10-18 21:32:29 -0400

    fixed request jade and added requests

--- request.py
+++ request.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, g, redirect, request, session, flash, Blueprint
 from sqlalchemy.sql import text
+from main import db
 
 request = Blueprint('request', __name__)
 
+@request.route('/create', methods=('GET', 'POST'))
+def create_service_request():
+	if request.method == 'POST':
+		sql = text('''insert into service_request(client_id, address, title, decsription, schedule)values('{0}', '{1}', '{2}');'''\
+		.format(session["user"], request.form.get('address'), request.form.get('title'), request.form.get('description'), request.form.get("time"))
+		db.engine.execute(sql)
+		return redirect('/')
