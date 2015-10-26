1919d8562532d3b5245970fd0d32faf05f3a2289
Vince <vcap51@yahoo.com>
2015-10-26 00:04:12 -0500

    fixed contracts and added pending support

--- views/contract.py
+++ views/contract.py
@@ -6,23 +6,39 @@ contractBP = Blueprint('contract', __name__, template_folder=app.template_folder
 
 @contractBP.route('/contracts/<contract_id>')
 def viewContract(contract_id):
-	sql = text('''SELECT client_username, worker_username, address, start_time, payment
-				  FROM contract
-				  WHERE contract_id=:contract_id''')
+	sql = text('''SELECT * FROM contract WHERE contract_id=:contract_id''')
 	result = db.engine.execute(sql, contract_id=contract_id)
 	result = result.fetchone()
 	if result:
-		return render_template('contract.jade', title='Title', client_username=result[0], worker_username=result[1], address=result[2], start_time=result[3], money=result[4])
+		return render_template('contract.jade',results=result)
 	return redirect('/')
 
 @contractBP.route('/contracts')
 def viewContracts():
 	if session.get('user'):
-		sql = text('''SELECT * FROM contract 
-					WHERE client_username=:username
-					OR worker_username=:username;''')
+		sql=text('''SELECT * FROM contract c, service_request sr WHERE c.service_id=sr.service_id AND (client_username=:username OR worker_username=:username) AND contract_status='accepted';''')
 		results = db.engine.execute(sql, username=session.get('user'))
 		results = results.fetchall()
 		return render_template('viewContracts.jade', results=results)
 	else:
 		return redirect('/')
+
+@contractBP.route('/pendingContracts')
+def viewPendingContracts():
+	user = session.get('user')
+	if user:
+		sql=text('''SELECT * FROM contract c, service_request sr WHERE c.service_id=sr.service_id AND (client_username=:username OR worker_username=:username) AND contract_status='pending';''')
+		results = db.enginge.execute(sql, username=user)
+		results = results.fetchall()
+		return render_template('viewPendingContracts.jade', results=results)
+	else:
+		return redirect('/')
+
+@contractBP.route('/pendingContracts/<contract_id>')
+def viewPendingContract(contract_id):
+	sql = text('''SELECT * FROM contract WHERE contract_id=:contract_id''')
+	result = db.engine.execute(sql, contract_id=contract_id)
+	result = result.fetchone()
+	if result:
+		return render_template('pendingContract.jade',results=results)
+	return redirect('/')
