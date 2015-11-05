4f7b545b1c73578bedcc3a74fdd071286782e813
Michael <mwhitman95@gmail.com>
2015-11-05 17:43:45 -0600

    colon.

--- views/rating.py
+++ views/rating.py
@@ -4,9 +4,9 @@ from . import db, app
 
 ratings = Blueprint('rating', __name__, template_folder=app.template_folder+'/rating')
 
-@ratings('contracts/<contract_id>/rating', methods=('GET', 'POST'))
+@ratings.route('/contracts/<contract_id>/rating', methods=('GET', 'POST'))
 def rateWorker(contract_id):
-	if request.method=="GET"
+	if request.method=="GET":
 		sql=text('''SELECT worker_username, contract_status, rating FROM contract WHERE contract_id=:id;''')
 		contract = db.engine.execute(sql, id=contract_id)
 		contract=contract.fetchone()
@@ -14,7 +14,7 @@ def rateWorker(contract_id):
 			return render_template('rating.jade', name=worker_username, id=contract_id)
 		else:
 			return redirect('/')
-	if request.method=="POST"
+	if request.method=="POST":
 		sql=text('''INSERT INTO table (:rating, :review) VALUES (rating, review);''')
 		result=db.engine.execute(sql, rating=request.form.get("rating"), review=request.form.get("review"))
 		flash("Rating Submitted")
