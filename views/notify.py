a9c67f64a0993a4b8dbc7c4b306d85c92f3b8cfd
Michael <mwhitman95@gmail.com>
2015-11-15 18:05:51 -0600

    Fixed timestamp formatting, order of notifications

--- views/notify.py
+++ views/notify.py
@@ -15,13 +15,15 @@ def pushNotification(name, message, link):
 def viewNotifications():
 	if session.get('user'):
 		#Get all new notifications
-		sql = text('''SELECT message, link, time FROM notification
-					  WHERE name=:user AND viewed=FALSE''')
+		sql = text('''SELECT message, link, to_char(time AT TIME ZONE 'CDT', 'MM/DD/YY HH12:MIAM') FROM notification
+					  WHERE name=:user AND viewed=FALSE
+					  ORDER BY time desc;''')
 		results = db.engine.execute(sql, user=session.get('user'))
 		newNotifications = results.fetchall()
 		#Get all old notifications
-		sql = text('''SELECT message, link, time FROM notification
-					  WHERE name=:user AND viewed=TRUE''')
+		sql = text('''SELECT message, link, to_char(time AT TIME ZONE 'CDT', 'MM/DD/YY HH12:MIAM') FROM notification
+					  WHERE name=:user AND viewed=TRUE
+					  ORDER BY time desc;''')
 		results = db.engine.execute(sql, user=session.get('user'))
 		oldNotifications = results.fetchall()
 		#Set new notifications as viewed now
