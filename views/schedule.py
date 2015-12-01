2491cd6d04c6528420d1379a0ca734990467526c
Arkojit <arkojit@hotmail.com>
2015-11-30 23:44:03 -0600

    Added abstraction functions for generating best fit worker list

--- views/schedule.py
+++ views/schedule.py
@@ -5,47 +5,6 @@ from . import db, app
 
 scheduler = Blueprint('scheduler', __name__, template_folder=app.template_folder+'/scheduler')
 
-def calculateScheduleCompatibility(workerScheduleArray, requestScheduleArray):#assume {Monday:3pm, Monday:4pm, Monday:5pm......etc
-	numbers = {} #return values. just counts how many times in common for each day {Monday: 4, Tuesday:2 .......etc
-	requestTimeSlots = len(requestScheduleArray)
-	for requestTime in workerScheduleArray:
-		for workerTime in requestScheduleArray:
-			if(requestTime[0] ==  workerTime[0] and requestTime[1] ==  workerTime[1]):
-				if requestTime[0] in numbers:
-					numbers[requestTime[0]] += 1
-				else:
-					numbers[requestTime[0]] = 1
-				break
-
-	return numbers, requestTimeSlots
-	print (requestTimeSlots)
-
-def calculateScheduleNumber(numbers, requestTimeSlots):
-	#do math here and get a number we can use to tell how relatable they are or relatable in comparison to other workers
-	workerTimeSlots = 0;
-	for value in numbers:
-		workerTimeSlots =+ value
-	return workerTimeSlots/requestTimeSlots
-
-def getSchedules(worker_username, service_id):
-	workerScheduleArray = []
-	requestScheduleArray = []
-	sql = text('''SELECT day, hour FROM worker_schedule WHERE
-				  worker_username=:worker;''')
-	schedule = db.engine.execute(sql, worker=worker_username)
-	schedule = schedule.fetchall()
-	for tuple in schedule:
-		workerScheduleArray.append([tuple[0], tuple[1]])
-	sql = text('''SELECT day, hour FROM service_schedule WHERE
-				  service_id=:request;''')
-	schedule = db.engine.execute(sql, request=service_id)
-	schedule = schedule.fetchall()
-	for tuple in schedule:
-		requestScheduleArray.append([tuple[0], tuple[1]])
-		print(requestScheduleArray[-1])
-
-	return workerScheduleArray, requestScheduleArray
-
 @scheduler.route('/schedule', methods=('GET', 'POST'))
 def setSchedule():
 	if not session.get('user') or session['type'] != 'worker':
